f87cbb5caff32f6618296fa8f8bf05dbee8fcbd6
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-12 01:04:54 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
+require 'pry'
 def unsafe?(speed)
+ 
+if speed >= 60 || speed <= 40
+  "true"
+else
+  "false"
 
+ 
 end
 
 
 
 def not_safe?(speed)
-	
+speed? >=60 || speed <= 40 : false 
 end
 	
 
